6c3d3fe37bb2d4c7a830117244d0010df294c63b
Juan David Cepeda <programadorcepeda@gmail.com>
2025-01-29 11:39:41 -0500

    Completed project

--- script.js
+++ script.js
@@ -2,5 +2,28 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+function generateTarget(){
+    return Math.floor(Math.random()*9 + 1);
+}
 
+function compareGuesses(human, pc, target){
+    let humanDifference = Math.abs(target-human);
+    let pcDifference = Math.abs(target-pc);
+    if (humanDifference <= pcDifference){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const updateScore = winner => {
+    if (winner === 'human'){
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore ++;
+    }
+} 
+
+const advanceRound = () =>{
+    currentRoundNumber++;
+}
